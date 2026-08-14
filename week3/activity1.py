b5731d125c0cf60154e277639f31fa5b45d6cc32
Aleksandr Voitenko <aleksandr.voitenko1986@gmail.com>
2026-08-15 09:53:13 +1200

    Minor code cleanup

--- week3/activity1.py
+++ week3/activity1.py
@@ -6,9 +6,9 @@ def main():
   iris = fetch_ucirepo(id=53)
 
   # Explicit types help VS Code/Pylance provide autocomplete
-  features: pd.DataFrame = iris.data.features
   targets: pd.DataFrame = iris.data.targets
 
+  ## Processing names
   flower_names = targets["class"].dropna().unique().tolist()
 
   print(f"Number of different flowers: {len(flower_names)}")
